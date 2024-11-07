239be007624846588801128efd49c17e060e81b0
Jemko <jemko23@t-online.de>
2024-11-08 00:52:14 +0100

    idk

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace StudioDotNet
 {
-    partial class Form1
+    partial class StudioForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1684, 973);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // StudioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(1708, 997);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "StudioForm";
+            this.Text = "Studio.net";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,32 @@
-﻿using System;
+﻿using StudioDotNet.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StudioDotNet
 {
-    public partial class Form1 : Form
+    public partial class StudioForm : Form
     {
-        public Form1()
+        private int zoom = 16;
+        public StudioForm()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Image image = new Bitmap(Resources.BG, new Size(32 * zoom / 16, 32 * zoom / 16));
+            TextureBrush tBrush = new TextureBrush(image);
+            Pen blackPen = new Pen(Color.Black);
+            e.Graphics.FillRectangle(tBrush, new Rectangle(0, 0, StudioForm.ActiveForm.Width, StudioForm.ActiveForm.Height));
         }
     }
 }
